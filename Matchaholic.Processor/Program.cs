8db0ad5d60154b1cf0b4c1670ff94b42f5c8a62f
Gokalp <GAcik@GACIKLPW10>
2023-03-11 19:43:24 +0100

    initial commit for worker service

---
+++ Matchaholic.Processor/Program.cs
@@ -0,0 +1,10 @@
+using Matchaholic.Processor;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
